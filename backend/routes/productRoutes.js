edaa80bb5d5717929e8b5a33b3101a1fcdb59119
Ilydanyxx <danya.chuyko@icloud.com>
2025-04-26 21:21:19 +0300

    c

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const { addProduct, getProducts } = require('../controllers/productController');
 const { verifyAdmin } = require('../middleware/authMiddleware');
 
-router.post('/add', addProduct); // без verifyAdmin
-
+router.post('/add', verifyAdmin, addProduct);
 router.get('/', getProducts);
 
 module.exports = router;
