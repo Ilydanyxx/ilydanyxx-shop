a63368cbbc2593ba9754b773ed19b5baf29e647e
Ilydanyxx <danya.chuyko@icloud.com>
2025-04-26 21:00:09 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
